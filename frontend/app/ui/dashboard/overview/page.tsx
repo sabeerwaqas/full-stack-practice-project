dc993ca018ff0747b07d5f299517108f7d2443d6
sabeer-ivl <sabeer.waqas@indusvalleylabs.com>
2026-01-08 12:07:00 +0500

    chore: solve build fail issues

--- frontend/app/ui/dashboard/overview/page.tsx
+++ frontend/app/ui/dashboard/overview/page.tsx
@@ -1,14 +1,12 @@
+import { fetchCardData, fetchRevenue } from '@/app/lib/data';
 import { Card } from '@/app/ui/dashboard/cards';
-import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
-import { fetchLatestInvoices, fetchCardData, fetchRevenue } from '@/app/lib/data';
-import { Suspense } from 'react';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import { RevenueChartSkeleton } from '@/app/ui/skeletons';
-import { generateYAxis } from '@/app/lib/utils';
+import { Suspense } from 'react';
  
 export default async function Page() {
-  const latestInvoices = await fetchLatestInvoices();
-   const revenue = await fetchRevenue(); 
+  const revenue = await fetchRevenue(); 
   const {
     numberOfInvoices,
     numberOfCustomers,
@@ -16,9 +14,6 @@ export default async function Page() {
     totalPendingInvoices,
   } = await fetchCardData();
 
-  
-  const chartHeight = 350;
-  const { yAxisLabels, topLabel } = generateYAxis(revenue || []);
  
   if (!revenue || revenue.length === 0) {
     return <p className="mt-4 text-gray-400">No data available.</p>;
@@ -41,9 +36,9 @@ export default async function Page() {
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
-          <RevenueChart revenue={revenue} />
+          <RevenueChart/>
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <LatestInvoices />
       </div>
     </main>
   );
