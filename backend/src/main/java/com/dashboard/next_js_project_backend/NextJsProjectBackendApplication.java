1f64d2650b6592c945afcbbc788f4cfba98dfc31
Sabeerwaqas <sabeerwaqas050@gmail.com>
2026-01-11 14:13:33 +0500

    chore: initialized backend project

---
+++ backend/src/main/java/com/dashboard/next_js_project_backend/NextJsProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dashboard.next_js_project_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextJsProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextJsProjectBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dashboard/next_js_project_backend/NextJsProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboard.next_js_project_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextJsProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
