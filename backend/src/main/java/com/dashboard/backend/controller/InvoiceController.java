dd4f5b7986d48b65da4f14e955b75078182a06ef
Sabeerwaqas <sabeerwaqas050@gmail.com>
2026-02-10 23:52:55 +0500

    refactor: format controllers

--- backend/src/main/java/com/dashboard/backend/controller/InvoiceController.java
+++ backend/src/main/java/com/dashboard/backend/controller/InvoiceController.java
@@ -4,10 +4,8 @@ import com.dashboard.backend.dto.InvoiceCountDTO;
 import com.dashboard.backend.dto.InvoiceDTO;
 import com.dashboard.backend.dto.PaidAmountDTO;
 import com.dashboard.backend.dto.PendingAmountDTO;
-import com.dashboard.backend.service.CustomerService;
 import com.dashboard.backend.service.InvoiceService;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,6 +14,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/invoice")
 public class InvoiceController {
+    
     private final InvoiceService service;
 
     public InvoiceController(InvoiceService service) {
@@ -23,32 +22,32 @@ public class InvoiceController {
     }
 
     @GetMapping
-    public List<InvoiceDTO> getInvoice(){
+    public List<InvoiceDTO> getInvoice() {
         return service.getInvoices();
     }
 
     @GetMapping("/count")
-    public InvoiceCountDTO getTotalInvoices(){
+    public InvoiceCountDTO getTotalInvoices() {
         return service.getInvoicesCount();
     }
 
     @GetMapping("/pending-amount")
-    public PendingAmountDTO getPendingAmount(){
+    public PendingAmountDTO getPendingAmount() {
         return service.getPendingAmount();
     }
 
     @GetMapping("/paid-amount")
-    public PaidAmountDTO getPaidAmount(){
+    public PaidAmountDTO getPaidAmount() {
         return service.getPaidAmount();
     }
 
     @PostMapping
-    public InvoiceDTO addInvoice(@Valid @RequestBody InvoiceDTO dto){
+    public InvoiceDTO addInvoice(@Valid @RequestBody InvoiceDTO dto) {
         return service.addInvoice(dto);
     }
 
     @PutMapping
-    public InvoiceDTO updateEntity(@Valid @RequestBody InvoiceDTO dto){
+    public InvoiceDTO updateEntity(@Valid @RequestBody InvoiceDTO dto) {
         return service.updateInvoice(dto);
     }
 
@@ -56,5 +55,4 @@ public class InvoiceController {
     public void deleteInvoice(@PathVariable UUID invoiceId) {
         service.deleteInvoice(invoiceId);
     }
-
 }
